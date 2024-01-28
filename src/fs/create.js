25773dbb2e363e48a93ef4b9cc54f6eef07228e7
Yelyzaveta <liza.vasilenko13@gmail.com>
2024-01-28 19:46:35 +0100

    fix: fix bugs

--- src/fs/create.js
+++ src/fs/create.js
@@ -3,7 +3,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const create = async () => {
-  const __dirname = path.dirname(fileURLToPath(import.meta.url)) + path.join('files');
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
   const fileName = 'fresh.txt';
   const filePath = path.join(__dirname, 'files', fileName);
   const fileContent = 'I am fresh and young\n';
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,6 @@
 import fs from 'fs/promises';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 const rename = async () => {
   const __dirname = path.dirname(fileURLToPath(import.meta.url));
